269df01c491b2a1731f11357f4d7fed41399a590
Riteek Rakesh <riteek7.12@gmail.com>
2024-07-04 18:57:26 +0530

    First commit

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import {Strategy, ExtractJwt} from "passport-jwt"
+import { DatabaseService } from "src/database/database.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(
+        private readonly  databaseService: DatabaseService
+    ){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: "Secret"
+        })
+    }
+
+    async validate(payload: { userEmail: string }) {
+        const user = await this.databaseService.user.findFirst({
+          where: {
+            email: payload.userEmail,
+          }
+        });
+        return user;
+      }
+
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+  const config = new DocumentBuilder()
+  .setTitle('Todo REST API')
+  .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. THe API is made using NestJs.")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
